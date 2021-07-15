5761da79962ce25613181e731ede5594ce854077
SIMjeahune <doctorsim1@naver.com>
2021-07-15 13:03:00 +0900

    3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like fafdjeahune!');
+    res.send('I like cat!');
 });
 
 app.get('/start', function(req, res){
