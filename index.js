9a7beef58314ac0e5bf7998c312bfc016d8bb219
SIMjeahune <doctorsim1@naver.com>
2021-07-15 15:23:34 +0900

    7rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('why so serious6');
+    res.send('why so serious7');
 });
 
 app.get('/start', function(req, res){
